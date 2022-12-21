fdce404184054ca2affbc771c0a9b99c3be0451a
jxy <lmx01036327@alibaba-inc.com>
2022-12-21 11:27:50 +0800

    推送消息格式化

--- main.py
+++ main.py
@@ -47,7 +47,10 @@ async def getGoodsList(q: str = Query(default=None, min_length=2, max_length=20)
             print(e)
             await redis.delete(item.decode())
             continue
-
+    # 根据价格排序
+    # res = sorted(res, key=lambda x: float(x.get("price")))
+    # 根据时间排序
+    res = sorted(res, key=lambda x: time.mktime(time.strptime(x.get("publish_time"), "%Y-%m-%d %H:%M:%S")), reverse=True)
     # 分页返回数据
     start = (page - 1) * page_size
     end = page * page_size
@@ -65,11 +68,20 @@ async def task(keywords):
                 data = await api.search(keyword)
                 # 存入redis
                 for item in data:
-                    if not await redis.exists(item.get("itemId")) and 4000 > int(item.get("price")) > 1000 \
+                    if not await redis.exists(item.get("itemId")) and 4000 > float(item.get("price")) > 1000 \
                             and "回收" not in item.get("title") and "求购" not in item.get("title")\
                             and time.mktime(time.strptime(item.get("publish_time"), "%Y-%m-%d %H:%M:%S")) > \
                             time.time() - 3600 * 24 * 3:
-                        await ding_push(item)
+                        # {"itemId":"695010780965","userNick":"薄利多销小店","price":"3888","publish_time":"2022-12-19
+                        # 23:47:03","title":"苹果12promax，外版无锁128功能全好面容秒解，成色如 图新原装机。屏幕上角小老化如图不明显，朋友自用机闲置低价出。",
+                        # "pic_url":"http://img.alicdn.com/bao/uploaded/i2/O1CN01uNcGsu2CWLpwt92zT_!!0-fleamarket.jpg
+                        # "} 解析成md格式
+                        message = f"### {item.get('title')}\n" \
+                                    f"> 价格：{item.get('price')}\n" \
+                                    f"> 发布时间：{item.get('publish_time')}\n" \
+                                    f"> 商品链接：https://item.taobao.com/item.htm?id={item.get('itemId')}\n" \
+                                    f"> ![商品图片]({item.get('pic_url')})\n"
+                        await ding_push(message)
                     await redis.set(item.get("itemId"), json.dumps(item))
                 await asyncio.sleep(10)
         except Exception as e:
@@ -94,7 +106,7 @@ if __name__ == '__main__':
     # 在另一个线程中启动事件循环loop
     t = threading.Thread(target=start_loop, args=(new_loop,))  # 通过当前线程开启新的线程去启动事件循环
     t.start()
-    asyncio.run_coroutine_threadsafe(task(['dell r730']), new_loop)  # 在新的线程中启动事件循环
+    asyncio.run_coroutine_threadsafe(task(['iphone12']), new_loop)  # 在新的线程中启动事件循环
     # 主线程启动fastapi
     uvicorn.run(app)
     # 2022-12-20 16:00:00格式转成时间戳
